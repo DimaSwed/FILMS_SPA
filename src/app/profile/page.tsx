44e0c78f2540dd7ad6fdf320d1b28c3e51b05ab4
Dmitry Bulankin <swed88@mail.ru>
2024-11-05 09:11:33 +0300

    feat: добавлено отображение данных о пользовтаеле в разделе profile

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -4,8 +4,29 @@ import { FC } from 'react'
 import { Box, Typography, Avatar, Stack, Divider } from '@mui/material'
 import Head from 'next/head'
 import Link from 'next/link'
+import Image from 'next/image'
+import avatar from '/public/avatar.png'
+import { useGetUserDetailsQuery } from '@/common/services/moviesApiTMDB'
+import { useSelector } from 'react-redux'
+import { RootState } from '@/common/store'
 
 const UserProfile: FC = () => {
+  const sessionId = useSelector((state: RootState) => state.auth.sessionId)
+  const {
+    data: user,
+    error,
+    isLoading
+  } = useGetUserDetailsQuery(sessionId!, {
+    skip: !sessionId // пропускает запрос, если sessionId не определен
+  })
+
+  if (isLoading) return <Typography>Loading...</Typography>
+  if (error) {
+    const errorMessage =
+      'status' in error && error.data ? error.data.toString() : 'An error occurred'
+    return <Typography>Error: {errorMessage}</Typography>
+  }
+
   return (
     <>
       <Head>
@@ -21,18 +42,35 @@ const UserProfile: FC = () => {
         }}
       >
         <Box sx={{ textAlign: 'center', mb: 4 }}>
-          <Avatar
-            alt="User Avatar"
-            src="/images/avaswed.JPG"
-            sx={{ width: 100, height: 100, mx: 'auto', mb: 2 }}
-          />
-          <Typography variant="h4">Dima SWED</Typography>
+          {user?.avatar?.gravatar?.hash ? (
+            <Avatar
+              src={`https://www.gravatar.com/avatar/${user.avatar.gravatar.hash}`}
+              alt={`${user.name}'s avatar`}
+              style={{ margin: '0 auto 10px auto', width: '90px', height: '90px' }}
+            />
+          ) : (
+            <Avatar
+              src="/avatar.png"
+              alt="User Avatar"
+              sx={{ width: 100, height: 100, mx: 'auto', mb: '5px' }}
+            />
+          )}
+          <Typography variant="h4">{user?.name || 'Неизвестно'}</Typography>
           <Typography variant="body1" color="secondary.contrastText">
-            @dimaswed
+            ID пользователя: {user?.id}
           </Typography>
-          <Typography variant="body2">swed88@mail.ru</Typography>
+          <Typography variant="body2"> {user?.email && user.email}</Typography>
           <Typography variant="body2" mb={1}>
-            Авторизация с помощью :
+            Авторизация с помощью : {user?.tmdb?.avatar_path == null ? 'TMDB' : ''}
+          </Typography>
+          <Typography variant="body2" mb={1}>
+            Язык: {user?.iso_639_1}
+          </Typography>
+          <Typography variant="body2" mb={1}>
+            Страна: {user?.iso_3166_1}
+          </Typography>
+          <Typography variant="body2" mb={1}>
+            Имя пользователя: {user?.username}
           </Typography>
           <Typography variant="body2" color="secondary.contrastText">
             0 Подписчиков • 0 Подписок
